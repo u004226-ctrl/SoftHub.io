5c578fd81ef2102b1dfeb7d603833918d3854b05
platilich <u004226@gmail.com>
2026-02-06 21:35:44 +0300

    Третья стабильная версия

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let hidden_button = document.getElementById('Logo')
+let audio = document.querySelector('audio');
+
+let pycharm = document.getElementById('pycharm')
+let second_audio = document.getElementById('pay')
+
+let go = document.getElementById('go')
+let firefox = document.getElementById('firefox')
+
+hidden_button.addEventListener('click', function() {
+    audio.play();
+
+})
+
+pycharm.addEventListener('click', function() {
+    second_audio.play();
+    
+})
+
+
+go.addEventListener('click', function() {
+    window.open('https://go.dev/', '_blank')
+})
+
+
+firefox.addEventListener('click', function() {
+    window.open('https://firefox.com', '_blank')
+
+})
